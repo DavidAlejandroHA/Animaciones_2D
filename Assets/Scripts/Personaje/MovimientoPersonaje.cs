79ec230710af9e47fe40fc7b142dc049ce3afd17
ElRedstoniano <99730787+DavidAlejandroHA@users.noreply.github.com>
2024-02-23 13:57:43 +0000

    Animación de caminar + salto

--- Assets/Scripts/Personaje/MovimientoPersonaje.cs
+++ Assets/Scripts/Personaje/MovimientoPersonaje.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class MovimientoPersonaje : MonoBehaviour
 {
     private float velocidad = 8f;
-    private float horizontal;
+    private float movX;
+    private bool saltando;
+    //private float movY;
     private float fuerzaSalto = 16f;
-    private bool mirandoHaciaDerecha = true;
+    //private bool mirandoHaciaDerecha = true;
+    private SpriteRenderer spriteRenderer;
 
     public Animator animator;
 
@@ -17,22 +20,34 @@ public class MovimientoPersonaje : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
+        // Cogiendo el input
+        //float movX = Input.GetAxis("Horizontal");
+        movX = Input.GetAxisRaw("Horizontal");
+        //movY = Input.GetAxisRaw("Vertical");
+
+        if (tocandoSuelo())
+        {
+            saltando = false;
+        }
 
         if (Input.GetButton("Jump") && tocandoSuelo())
         {
+            saltando = true;
             rb.velocity = new Vector2(rb.velocity.x, fuerzaSalto);
 
             if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
             {
                 rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
             }
+            
+            
         }
         
         comprobarCambioLado();
@@ -41,7 +56,7 @@ public class MovimientoPersonaje : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * velocidad, rb.velocity.y);
+        rb.velocity = new Vector2(movX * velocidad, rb.velocity.y);
     }
 
     private bool tocandoSuelo()
@@ -51,18 +66,28 @@ public class MovimientoPersonaje : MonoBehaviour
 
     private void comprobarCambioLado()
     {
-        if (mirandoHaciaDerecha && horizontal < 0f || !mirandoHaciaDerecha && horizontal > 0f)
+        /*
+        if (mirandoHaciaDerecha && movX < 0f || !mirandoHaciaDerecha && movX > 0f)
         {
             mirandoHaciaDerecha = !mirandoHaciaDerecha;
             Vector3 localScale = transform.localScale;
             localScale.x = -localScale.x;
             transform.localScale = localScale;
+        }*/
+        if (movX < 0f)
+        {
+            spriteRenderer.flipX = true;
+        } else if (movX > 0f)
+        {
+            spriteRenderer.flipX = false;
         }
     }
 
     private void establecerAnimaciones()
     {
         animator.SetFloat("VelY", rb.velocity.y);
-        animator.SetBool("VelX", rb.velocity.y != 0f);
+        animator.SetBool("VelX", rb.velocity.x != 0f && 
+            (rb.velocity.x >= 0.1f || rb.velocity.x <= -0.1f));
+        animator.SetBool("Saltando", saltando);
     }
 }
